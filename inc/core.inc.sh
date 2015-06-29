1e5b323b855f384c9612be24cd9ea24f80764e72
DeaDSouL <DeaDSouL@users.noreply.github.com>
2015-06-29 14:58:45 +0300

    Adding versions comparison functions

--- inc/core.inc.sh
+++ inc/core.inc.sh
@@ -5,6 +5,8 @@
 
 
 
+# ------------------------------------------------------------------------------
+
 # Check whether it is good to proceed
 function __isit_good2go() {
 	if [[ ("$_iam" == "root") && ("$_scriptis" != 'safe2run' || -z "$_username") ]]; then
@@ -23,6 +25,7 @@ function __isit_good2go() {
 	fi
 }
 
+# ------------------------------------------------------------------------------
 
 # How should we proceed?
 function __is_deps_ok() {
@@ -51,6 +54,7 @@ function __is_deps_ok() {
     done
 }
 
+# ------------------------------------------------------------------------------
 
 # Custom exit function
 function __q() {
@@ -59,6 +63,7 @@ function __q() {
 	exit $1
 }
 
+# ------------------------------------------------------------------------------
 
 # Aborting function: Should abort the sub-script process, if anything goes wrong
 # TODO: see if there is a way to implement this to make force the function to exit
@@ -68,6 +73,7 @@ function __a() {
 	return 1
 }
 
+# ------------------------------------------------------------------------------
 
 # Making & checking the existence of the temp dir we're going to use
 function __mktemp_d() {
@@ -75,6 +81,52 @@ function __mktemp_d() {
     [[ `pwd -P` != "$_tmp_dir" ]] && cd "$_tmp_dir"
 }
 
+# ------------------------------------------------------------------------------
+
+# URL: http://stackoverflow.com/a/4025065
+# We should not call it directly, instead we should use `__need2update $1 $2`
+function __ver_comp () {
+    if [[ $1 == $2 ]]; then
+        return 0
+    fi
+    local OIFS=$IFS
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]})); then
+            IFS=$OIFS
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
+            IFS=$OIFS
+            return 2
+        fi
+    done
+    return 0
+}
+
+# ------------------------------------------------------------------------------
+
+# usage ex: [[ `__need2update $1 $2` == 1 ]] && echo 'yes' || echo 'no'
+function __need2update() {
+    # $1 : installed version
+    # $2 : avilable version
+    __ver_comp $1 $2
+    [[ $? == 2 ]] && echo 1 || echo 0
+}
+
+# ------------------------------------------------------------------------------
 
 # Called as: __in_array "keyword" "${array_name[@]}"
 function __in_array() {
@@ -83,6 +135,7 @@ function __in_array() {
 	return 1 # not found
 }
 
+# ------------------------------------------------------------------------------
 
 # TODO: Need to make it works
 function __is_array() {
@@ -90,6 +143,7 @@ function __is_array() {
 	#declare -p non-existing-var 2> /dev/null | grep -q '^declare \-a' && echo array || echo no array
 }
 
+# ------------------------------------------------------------------------------
 
 # called: __write_file "contents" "path/to/file"
 # TODO: make sure file is writable.
@@ -103,3 +157,5 @@ function __write_file() {
 		echo 'Exiting..'; __q 1
 	fi
 }
+
+# ------------------------------------------------------------------------------
